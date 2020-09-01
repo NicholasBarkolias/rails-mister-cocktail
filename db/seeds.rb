1aae6f4272651073d07363d1edb5b598f8d46ea2
Nicholas Barkolias <nbarkolias@gmail.com>
2020-09-01 22:37:36 +1000

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+user['drinks'].each do |drink|
+Ingredient.create(name: drink['strIngredient1'])
+end
 
 Ingredient.create(name: "Coke")
 Ingredient.create(name: "Tonic")
