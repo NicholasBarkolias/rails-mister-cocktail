fc151551603bddb0535d60fcf546a14d2e400eb1
Nicholas Barkolias <nbarkolias@gmail.com>
2020-09-01 22:17:32 +1000

    simple form select2 removed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require ("select2")
-require ('select2_simple_form')
+
 
 import 'bootstrap';
 
